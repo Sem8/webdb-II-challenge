e0be5b58743b785b9751fb90779ad7ce98c8b747
Sem8 <mssemmi8@gmail.com>
2019-03-26 17:10:31 -0400

    MVP

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const zoosRouter = require('./zoos/zoos-router');
+
+// const router = require('express').Router();
+
+// const knex = require('knex');
+
+// const knexConfig = {
+//   client: "sqlite3",
+//   useNullAsDefault: true,
+//   connection: {
+//     filename: "./data/lambda.sqlite3"
+//   },
+// };
+
+// const db = knex(knexConfig);
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+server.use('/api/zoos', zoosRouter);
+
 // endpoints here
+
+// router.get("/", (req, res) => {
+//   // db('zoos').then(zoos => {
+//   //   res.status(200).json(zoos)
+//   // }).catch(error => {
+//   //   res.status(500).json(error);
+//   // });
+//   res.send('get alls zoos data here');
+// });
 
 const port = 3300;
 server.listen(port, function() {
